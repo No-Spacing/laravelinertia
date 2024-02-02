0147a1dcbbed10ce58e4f37104193f48136e6f5d
No-Spacing <64142238+No-Spacing@users.noreply.github.com>
2024-02-02 18:55:20 +0800

    remove button from toolbar

--- resources/js/Layout/App.vue
+++ resources/js/Layout/App.vue
@@ -5,16 +5,7 @@
                 color="primary"
             >
                 <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
                 <v-toolbar-title>Laravel - Inertiajs</v-toolbar-title>
-
-                <v-spacer></v-spacer>
-
-                <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-                <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-                <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
             </v-app-bar>
 
             <v-navigation-drawer
